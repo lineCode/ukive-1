811afdd709b594b609e9ef99e1b8d30f264138a7
ucclkp <ucclkp@163.com>
2017-11-07 19:06:06 +0800

    修改工程属性

---
+++ ukive/application.cpp
@@ -0,0 +1,125 @@
+﻿#include "application.h"
+
+#include <Windows.h>
+
+
+namespace ukive {
+
+    bool Application::sVSyncEnabled = true;
+
+
+    Application::Application() {
+        initApplication();
+    }
+
+    Application::Application(wchar_t *cl) {
+        parseCommandLine(cl);
+        initApplication();
+    }
+
+    Application::Application(int argc, wchar_t *argv[]) {
+        for (int i = 0; i < argc; ++i)
+            command_list_.push_back(argv[i]);
+        initApplication();
+    }
+
+
+    Application::~Application() {
+        cleanApplication();
+    }
+
+
+    void Application::initApplication() {
+        quit_strategy_ = QUIT_WHEN_STARTUP_WINDOW_CLOSED;
+    }
+
+    void Application::cleanApplication() {
+    }
+
+    void Application::parseCommandLine(wchar_t *cmdLine) {
+        if (cmdLine == nullptr)
+            return;
+
+        std::wstring cmdString = cmdLine;
+        if (cmdString.empty())
+            return;
+
+        size_t i = cmdString.find(L" ");
+        if (i == std::wstring::npos)
+        {
+            command_list_.push_back(cmdString);
+            return;
+        }
+
+        size_t newStart = 0;
+
+        while (i != std::wstring::npos) {
+            std::wstring tmp = cmdString.substr(newStart, i - newStart);
+            command_list_.push_back(tmp);
+
+            newStart = i + 1;
+            i = cmdString.find(L" ", newStart);
+            if (i == std::wstring::npos) {
+                tmp = cmdString.substr(newStart, cmdString.length() - newStart);
+                command_list_.push_back(tmp);
+            }
+        }
+    }
+
+
+    void Application::run() {
+        MSG msg;
+        BOOL ret;
+        ZeroMemory(&msg, sizeof(MSG));
+
+        while ((ret = ::GetMessageW(&msg, NULL, 0, 0)) != 0)
+        {
+            if (ret == -1) {
+                // Error.
+            }
+            else
+            {
+                ::TranslateMessage(&msg);
+                ::DispatchMessageW(&msg);
+            }
+        }
+    }
+
+
+    std::wstring Application::getCommand(size_t index) {
+        return command_list_.at(index);
+    }
+
+
+    size_t Application::getCommandCount() {
+        return command_list_.size();
+    }
+
+
+    void Application::setQuitStrategy(QuitStrategy qs) {
+        quit_strategy_ = qs;
+    }
+
+
+    Application::QuitStrategy Application::getQuitStrategy() {
+        return quit_strategy_;
+    }
+
+    void Application::setVSync(bool enable){
+        sVSyncEnabled = enable;
+    }
+
+    bool Application::isVSyncEnabled(){
+        return sVSyncEnabled;
+    }
+
+
+    int Application::getScreenWidth(){
+        return ::GetSystemMetrics(SM_CXSCREEN);
+    }
+
+    int Application::getScreenHeight(){
+        return ::GetSystemMetrics(SM_CYSCREEN);
+    }
+
+}
---
+++ ukive/application.h
@@ -0,0 +1,50 @@
+﻿#ifndef UKIVE_APPLICATION_H_
+#define UKIVE_APPLICATION_H_
+
+#include <string>
+#include <vector>
+
+
+namespace ukive {
+
+    class Application
+    {
+    public:
+        Application();
+        Application(wchar_t *cl);
+        Application(int argc, wchar_t *argv[]);
+        ~Application();
+
+        enum QuitStrategy {
+            QUIT_WHEN_LAST_WINDOW_CLOSED,
+            QUIT_WHEN_STARTUP_WINDOW_CLOSED,
+        };
+
+        void run();
+
+        std::wstring getCommand(size_t index);
+        size_t getCommandCount();
+
+        void setQuitStrategy(QuitStrategy qs);
+        QuitStrategy getQuitStrategy();
+
+        static int getScreenWidth();
+        static int getScreenHeight();
+
+        static void setVSync(bool enable);
+        static bool isVSyncEnabled();
+
+    private:
+        void initApplication();
+        void cleanApplication();
+        void parseCommandLine(wchar_t *cmdLine);
+
+        static bool sVSyncEnabled;
+
+        QuitStrategy quit_strategy_;
+        std::vector<std::wstring> command_list_;
+    };
+
+}
+
+#endif  // UKIVE_APPLICATION_H_
