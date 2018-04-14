69d69f90f90db5eec1934ec7d800e03a515172a0
ucclkp <ucclkp@163.com>
2018-04-14 23:00:58 +0800

    Modify logging macros

--- ukive/log.h
+++ ukive/log.h
@@ -7,24 +7,38 @@
 #include "utils/string_utils.h"
 
 
-#define LOG(level) ukive::Log(__FILE__, __LINE__, level).stream()
+#define LOG_STREAM(level) \
+    ::ukive::Log(__FILE__, __LINE__, level).stream()
 
-#define CHECK(exp) { \
-    bool result = (exp); \
-    if (!result) { ukive::Log::debugBreak(); } \
-}
+#define VOIDABLE_STREAM(stream, condition) \
+    !(condition) ? (void) 0 : ::ukive::LogVoidify() & (stream)
+
+#define IS_LOG_ON   true
+#define IS_CHECK_ON true
 
 #ifndef NDEBUG
 
-#define DLOG(level) LOG(level)
-#define DCHECK(exp) CHECK(exp)
+#define IS_DLOG_ON   true
+#define IS_DCHECK_ON true
 
 #else
 
-#define DLOG(level)
-#define DCHECK(exp)
+#define IS_DLOG_ON   false
+#define IS_DCHECK_ON false
 
 #endif
+
+#define LOG(level) \
+    VOIDABLE_STREAM(LOG_STREAM(level), IS_LOG_ON)
+
+#define CHECK(condition) \
+    VOIDABLE_STREAM(LOG_STREAM(::ukive::Log::FATAL), IS_CHECK_ON && !(condition))
+
+#define DLOG(level) \
+    VOIDABLE_STREAM(LOG_STREAM(level), IS_DLOG_ON)
+
+#define DCHECK(condition) \
+    VOIDABLE_STREAM(LOG_STREAM(::ukive::Log::FATAL), IS_DCHECK_ON && !(condition))
 
 
 namespace ukive {
@@ -55,6 +69,12 @@ namespace ukive {
         int line_number_;
         std::string file_name_;
         std::ostringstream stream_;
+    };
+
+    class LogVoidify {
+    public:
+        LogVoidify() = default;
+        void operator&(std::ostream& s) {}
     };
 
 }
